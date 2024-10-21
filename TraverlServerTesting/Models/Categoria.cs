64ad2376c8749dea304b296160d53a96eb1004c3
SolveNaN <dagoberto6993@gmail.com>
2024-10-20 21:29:08 -0500

    Controladores y clases

---
+++ TraverlServerTesting/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿namespace TraverlServerTesting.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public Unidad? Unidad { get; set; }
+        public int? UnidadId { get; set; }
+        public string? Nombre { get; set; }      
+        public string? SubTitulo { get; set; }
+        public string? Tipo { get; set; } // para definir tipos de habitacion,plato,transporte
+        public string? Descripcion1 { get; set; }
+        public string? Descripcion2 { get; set; }
+        public string? Valor { get; set; }
+        public string? Estado { get; set; }//Disponible, No Disponible
+        public string? UrlImg1 { get; set; }
+        public string? UrlImg2 { get; set; }
+        public string? UrlImg3 { get; set; }
+        public string? UrlImg4 { get; set; }
+        public string? UrlImg5 { get; set; }
+    }
+}
