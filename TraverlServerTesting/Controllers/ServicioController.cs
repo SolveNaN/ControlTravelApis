bb4c04a72246645ed6585a94f78ae40317d54d4a
cawtoz <cawtoz.dev@gmail.com>
2024-10-22 18:07:57 -0500

    Added Controllers
    - UnidadController
    - CategoriaController
    - ServicioController

--- TraverlServerTesting/Controllers/ServicioController.cs
+++ TraverlServerTesting/Controllers/ServicioController.cs
@@ -1,236 +1,90 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using TraverlServerTesting.Data;
 using TraverlServerTesting.Models;
-using Microsoft.EntityFrameworkCore;
 namespace TraverlServerTesting.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ServicioController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ServicioRepository servicioRepository = new ServicioRepository();
 
-        public ServicioController(ApplicationDbContext context)
-        {
 
-            _context = context;
-        }
-        //Estado de conexion con el servidor
-        [HttpGet]
-        [Route("ConexionServidor")]
-        public async Task<ActionResult<string>> GetConexionServidor()
-        {
-            return Ok("Conectado");
-        }
-        //Estados de conexion con la tabla de la base de datos
-        [HttpGet]
-        [Route("ConexionDB")]
-        public async Task<ActionResult<string>> GetConexionDB()
-        {
-            try
-            {
-                var usuarios = await _context.Ejemplos.ToListAsync();
-                return Ok("Buena Calidad");
-
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Mala calidad");
-            }
-        }
-        //Aqui comienza el CRUD
+        // Obtener todos los servicios
         [HttpGet("Listado")]
-        public async Task<ActionResult<List<Ejemplo>>> GetEjemplos()
+        public ActionResult<List<Servicio>> GetServicios()
         {
-            var lista = await _context.Ejemplos.ToListAsync();
-            return Ok(lista);
-        }
-        //Aqui comienza el CRUD
-        [HttpGet("ListadoServicios")]
-        public async Task<ActionResult<List<Servicio>>> GetServicios()
-        {
-            var lista = new List<Servicio>()
-            {
-                new Servicio {
-                    Id = 1,
-                    Nombre="Atesanias La esquina",
-                    Descripcion1 ="Recuerdos para todos",
-                    UnidadId=1,
-
-                },
-                new Servicio {
-                    Id = 2,
-                    Nombre="Piezas arqueológicas",
-                    Descripcion1 =" Incluye urnas y vasijas con formas humanas y animales, así como trazos geométricos",
-                    UnidadId=7,
-                },
-                new Servicio {
-                    Id = 3,
-                    Nombre="Exposiciones permanentes",
-                    Descripcion1 ="Relatan la historia y cultura de la etnia indígena Guayupe",
-                    UnidadId=7,
-                },
-                new Servicio {
-                    Id = 4,
-                    Nombre="Recorridos guiados",
-                    Descripcion1 ="Ofrecen una visión detallada de las piezas y su contexto histórico",
-                    UnidadId=7,
-                },
-                new Servicio {
-                    Id = 5,
-                    Nombre="Sala de audiovisuales",
-                    Descripcion1 =" Presenta videos y presentaciones interactivas sobre la cultura Guayupe",
-                    UnidadId=7,
-                },
-                new Servicio {
-                    Id = 6,
-                    Nombre="Talleres educativos",
-                    Descripcion1 =" Actividades para niños y adultos que fomentan el aprendizaje sobre la historia local",
-                    UnidadId=7,
-                },
-                new Servicio {
-                    Id = 7,
-                    Nombre="Caño Cristales",
-                    Descripcion1 ="Conocido como el río de los cinco colores, ofrece vistas impresionantes con sus aguas multicolores",
-                    UnidadId=8,
-                },
-                new Servicio {
-                    Id = 8,
-                    Nombre="Senderismo",
-                    Descripcion1 ="Varias rutas de senderismo que permiten explorar la exuberante naturaleza del parque",
-                    UnidadId=8,
-                },
-                new Servicio {
-                    Id = 9,
-                    Nombre="Observación de aves",
-                    Descripcion1 ="Un paraíso para los observadores de aves, con una gran variedad de especies endémicas y migratorias",
-                    UnidadId=8,
-                },
-                new Servicio {
-                    Id = 10,
-                    Nombre="Cascadas y lagunas",
-                    Descripcion1 ="Diversas cascadas y lagunas que son perfectas para disfrutar de la naturaleza y tomar fotos",
-                    UnidadId=8,
-                },
-                new Servicio {
-                    Id = 11,
-                    Nombre="Ecoturismo",
-                    Descripcion1 =" Actividades como el baño pasivo y recreativo, interpretación del patrimonio arqueológico y fotografía de la flora y fauna",
-                    UnidadId=8,
-                },
-                new Servicio {
-                    Id = 12,
-                    Nombre="Habitación Doble",
-                    Descripcion1 ="Una habitación con una cama doble, ideal para parejas.",
-                    UnidadId=8,
-                },
-                new Servicio {
-                    Id = 13,
-                    Nombre="Habitación Triple",
-                    Descripcion1 ="Una habitación con una cama doble y una cama extra, perfecta para tres personas.",
-                    UnidadId=8,
-                },
-                new Servicio {
-                    Id = 14,
-                    Nombre="Habitación Familiar",
-                    Descripcion1 ="Una habitación más grande con una cama doble y una o más camas adicionales, adecuada para familias.",
-                    UnidadId=8,
-                },
-                new Servicio {
-                    Id = 15,
-                    Nombre="Suite Presidencial",
-                    Descripcion1 ="Una suite lujosa con mayores comodidades, incluyendo un área de estar y baño más grande.",
-                    UnidadId=8,
-                }
-            };
-            return Ok(lista);
+            return Ok(servicioRepository.GetAllServicios());
         }
 
+
+        // Obtener los servicios de una Unidad
         [HttpGet]
-        [Route("ConsutarId/{id}")]
-        public async Task<ActionResult<List<Ejemplo>>> GetSingleEjemplo(int id)
+        [Route("ListadoPorUnidad/{unidadId}")]
+        public ActionResult<List<Servicio>> GetServiciosByUnidad(int unidadId)
         {
-            var miobjeto = await _context.Ejemplos.FirstOrDefaultAsync(ob => ob.Id == id);
-            if (miobjeto == null)
+            var servicios = servicioRepository.GetServiciosByUnidadId(unidadId);
+            if (servicios == null || !servicios.Any())
             {
-                return NotFound(" :/");
+                return NotFound($"No se encontraron servicios para la unidad con ID {unidadId}.");
             }
 
-            return Ok(miobjeto);
+            return Ok(servicios);
         }
 
+
+        // Obtener los servicios de una Categoria
+        [HttpGet]
+        [Route("ListadoPorCategoria/{categoriaId}")]
+        public ActionResult<List<Servicio>> GetServiciosByCategoria(int categoriaId)
+        {
+            var servicios = servicioRepository.GetServiciosByCategoriaId(categoriaId);
+            if (servicios == null || !servicios.Any())
+            {
+                return NotFound($"No se encontraron servicios para la categoria con ID {categoriaId}.");
+            }
+
+            return Ok(servicios);
+        }
+
+
+        // Consultar un servicio
+        [HttpGet]
+        [Route("Consultar/{id}")]
+        public ActionResult<Servicio> GetServicioById(int id) 
+        {
+            var servicio = servicioRepository.GetServicioById(id);
+            if (servicio == null)
+            {
+                return NotFound("Servicio no encontrado.");
+            }
+
+            return Ok(servicio);
+        }
+
+
+        // Crear un nuevo servicio
         [HttpPost("Crear")]
-        public async Task<ActionResult<string>> CreateEjemplo(Ejemplo objeto)
+        public ActionResult<string> CreateServicio(Servicio servicio)
         {
-            try
-            {
-                _context.Ejemplos.Add(objeto);
-                await _context.SaveChangesAsync();
-                return Ok("Creado con éxio");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Error durante el proceso de almacenamiento");
-            }
-
-        }
-        [HttpPost("AgregarListado")]
-        public async Task<ActionResult<string>> AgregarListadoTanque(List<Ejemplo> listado)
-        {
-            try
-            {
-                foreach (var item in listado)
-                {
-                    var miobjeto = await _context.Ejemplos.FirstOrDefaultAsync(ob => ob.Nombre == item.Nombre);
-                    if (miobjeto == null)
-                    {
-                        _context.Ejemplos.Add(item);
-                        await _context.SaveChangesAsync();
-                    }
-                }
-                return Ok("Creado con éxio");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Error durante el proceso de almacenamiento");
-            }
-        }
-        [HttpPut("Actualizar/{id}")]
-        public async Task<ActionResult<List<Ejemplo>>> UpdateEjemplo(Ejemplo objeto)
-        {
-
-            var DbObjeto = await _context.Ejemplos.FindAsync(objeto.Id);
-            if (DbObjeto == null)
-                return BadRequest("no se encuentra");
-            //DbObjeto.Nombre = objeto.Nombre;
-            await _context.SaveChangesAsync();
-            return Ok(await _context.Ejemplos.ToListAsync());
+            servicioRepository.AddServicio(servicio);
+            return Ok("Servicio creado con éxito.");
         }
 
 
+        // Eliminar un servicio
         [HttpDelete]
         [Route("Eliminar/{id}")]
-        public async Task<ActionResult<string>> DeleteEjemplo(int id)
+        public ActionResult<string> DeleteServicio(int id)
         {
-            var DbObjeto = await _context.Ejemplos.FirstOrDefaultAsync(Ob => Ob.Id == id);
-            if (DbObjeto == null)
+            var servicio = servicioRepository.GetServicioById(id);
+            if (servicio == null)
             {
-                return NotFound("no existe :/");
+                return NotFound("Servicio no encontrado.");
             }
 
-            try
-            {
-                _context.Ejemplos.Remove(DbObjeto);
-                await _context.SaveChangesAsync();
-
-                return Ok("Eliminado correctamente");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("No fué posible eliminar el objeto");
-            }
-
+            servicioRepository.DeleteServicio(id);
+            return Ok("Servicio eliminado correctamente.");
         }
     }
 }
--- TraverlServerTesting/Controllers/UnidadController.cs
+++ TraverlServerTesting/Controllers/UnidadController.cs
@@ -3,227 +3,61 @@ using Microsoft.AspNetCore.Mvc;
 using TraverlServerTesting.Data;
 using TraverlServerTesting.Models;
 using Microsoft.EntityFrameworkCore;
+
 namespace TraverlServerTesting.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class UnidadController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        private readonly UnidadRepository unidadRepository = new UnidadRepository();
 
-        public UnidadController(ApplicationDbContext context)
-        {
 
-            _context = context;
-        }
-        //Estado de conexion con el servidor
-        [HttpGet]
-        [Route("ConexionServidor")]
-        public async Task<ActionResult<string>> GetConexionServidor()
-        {
-            return Ok("Conectado");
-        }
-        //Estados de conexion con la tabla de la base de datos
-        [HttpGet]
-        [Route("ConexionDB")]
-        public async Task<ActionResult<string>> GetConexionDB()
-        {
-            try
-            {
-                var usuarios = await _context.Ejemplos.ToListAsync();
-                return Ok("Buena Calidad");
-
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Mala calidad");
-            }
-        }
-        //Aqui comienza el CRUD
+        // Obtener todas las unidades
         [HttpGet("Listado")]
-        public async Task<ActionResult<List<Unidad>>> GetEjemplos()
+        public ActionResult<List<Unidad>> GetUnidades()
         {
-
-            var lista = new List<Unidad>
-            {
-                new Unidad
-    {
-                    Id = 1,
-        Titulo = "Parque Principal de Acacias",
-        Descripcion1 = "Un lugar ideal para relajarse y disfrutar del entorno.",
-        UrlImg1 = "assets/images/parque-principal-acacias.jpg",
-        Tipo = "Parque"
-    },
-    new Unidad
-    {
-         Id = 2,
-        Titulo = "Mirador Turístico de San Martín",
-        Descripcion1 = "Ofrece vistas panorámicas impresionantes.",
-        UrlImg1 = "assets/images/mirador-san-martin.jpg",
-        Tipo = "Mirador"
-    },
-    new Unidad
-    {
-         Id = 3,
-        Titulo = "Parque los Fundadores",
-        Descripcion1 = "Un parque histórico con áreas verdes y actividades recreativas.",
-        UrlImg1 = "assets/images/parque-los-fundadores.jpg",
-        Tipo = "Parque"
-    },
-    new Unidad
-    {
-         Id = 4,
-        Titulo = "Mirador la Piedra del Amor",
-        Descripcion1 = "Un mirador popular con vistas espectaculares.",
-        UrlImg1 = "assets/images/mirador-piedra-amor.jpg",
-        Tipo = "Mirador"
-    },
-    new Unidad
-    {
-         Id = 5,
-        Titulo = "Parque las Malocas",
-        Descripcion1 = "Un parque con espacios para deportes y actividades al aire libre.",
-        UrlImg1 = "assets/images/parque-las-malocas.jpg",
-        Tipo = "Parque"
-    },
-    new Unidad
-    {
-        Id = 6,
-        Titulo = "Monumentos de Villavicencio",
-        Descripcion1 = "Incluye varios monumentos históricos y culturales.",
-        UrlImg1 = "assets/images/monumentos-villavicencio.jpg",
-        Tipo = "Monumento"
-    },
-    new Unidad
-    {
-        Id = 7,
-        Titulo = "Museo Arqueológico Guayupe",
-        Descripcion1 = "Un museo que ofrece una visión de la historia y cultura local.",
-        UrlImg1 = "assets/images/museo-guayupe.jpg",
-        Tipo = "Museo"
-    },
-    new Unidad
-    {
-        Id = 8,
-        Titulo = "Parque Nacional Natural Sierra de La Macarena",
-        Descripcion1 = "Famoso por el Caño Cristales, un destino natural impresionante.",
-        UrlImg1 = "assets/images/parque-sierra-macarena.jpg",
-        Tipo = "Parque Nacional"
-    },
-    new Unidad
-    {
-        Id = 9,
-        Titulo = "Caño Cristales (Río de los Cinco Colores)",
-        Descripcion1 = "Un impresionante río conocido por sus aguas multicolores.",
-        UrlImg1 = "assets/images/cano-cristales.jpg",
-        Tipo = "Río"
-    },
-    new Unidad
-    {
-        Id = 10,
-        Titulo = "Ruta Turística del Meta",
-        Descripcion1 = "Una ruta que te lleva a varios destinos turísticos interesantes en la región.",
-        UrlImg1 = "assets/images/ruta-turistica-meta.jpg",
-        Tipo = "Ruta"
-    },
-    new Unidad
-    {
-        Id = 11,
-        Titulo = "Gran Hotel Acacias",
-        Descripcion1 = "Este hotel ofrece habitaciones cómodas con baño privado, TV satelital, aire acondicionado, escritorio y snacks. También cuenta con Wi-Fi en las habitaciones y áreas comunes",
-        UrlImg1 = "assets/images/ruta-turistica-meta.jpg",
-        Tipo = "Hoteles"
-    }
-            };
-            return Ok(lista);
-
+            return Ok(unidadRepository.GetAllUnidades());
         }
 
 
+        // Consultar una unidad
         [HttpGet]
-        [Route("ConsutarId/{id}")]
-        public async Task<ActionResult<List<Ejemplo>>> GetSingleEjemplo(int id)
+        [Route("Consultar/{id}")]
+        public async Task<ActionResult<Unidad>> GetUnidadById(int id)
         {
-            var miobjeto = await _context.Ejemplos.FirstOrDefaultAsync(ob => ob.Id == id);
-            if (miobjeto == null)
+            var unidad = unidadRepository.GetUnidadById(id);
+            if (unidad == null)
             {
-                return NotFound(" :/");
+                return NotFound("No se encontró la unidad.");
             }
 
-            return Ok(miobjeto);
+            return Ok(unidad);
         }
 
+
+        // Crear una nueva unidad
         [HttpPost("Crear")]
-        public async Task<ActionResult<string>> CreateEjemplo(Ejemplo objeto)
+        public async Task<ActionResult<string>> CreateUnidad(Unidad unidad)
         {
-            try
-            {
-                _context.Ejemplos.Add(objeto);
-                await _context.SaveChangesAsync();
-                return Ok("Creado con éxio");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Error durante el proceso de almacenamiento");
-            }
-
-        }
-        [HttpPost("AgregarListado")]
-        public async Task<ActionResult<string>> AgregarListadoTanque(List<Ejemplo> listado)
-        {
-            try
-            {
-                foreach (var item in listado)
-                {
-                    var miobjeto = await _context.Ejemplos.FirstOrDefaultAsync(ob => ob.Nombre == item.Nombre);
-                    if (miobjeto == null)
-                    {
-                        _context.Ejemplos.Add(item);
-                        await _context.SaveChangesAsync();
-                    }
-                }
-                return Ok("Creado con éxio");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Error durante el proceso de almacenamiento");
-            }
-        }
-        [HttpPut("Actualizar/{id}")]
-        public async Task<ActionResult<List<Ejemplo>>> UpdateEjemplo(Ejemplo objeto)
-        {
-
-            var DbObjeto = await _context.Ejemplos.FindAsync(objeto.Id);
-            if (DbObjeto == null)
-                return BadRequest("no se encuentra");
-            //DbObjeto.Nombre = objeto.Nombre;
-            await _context.SaveChangesAsync();
-            return Ok(await _context.Ejemplos.ToListAsync());
+            unidadRepository.AddUnidad(unidad);
+            return Ok("Unidad creada con éxito.");
         }
 
 
+        // Eliminar una unidad
         [HttpDelete]
         [Route("Eliminar/{id}")]
-        public async Task<ActionResult<string>> DeleteEjemplo(int id)
+        public async Task<ActionResult<string>> DeleteUnidad(int id)
         {
-            var DbObjeto = await _context.Ejemplos.FirstOrDefaultAsync(Ob => Ob.Id == id);
-            if (DbObjeto == null)
+            var unidadDb = unidadRepository.GetUnidadById(id);
+            if (unidadDb == null)
             {
-                return NotFound("no existe :/");
+                return NotFound("Unidad no encontrada.");
             }
 
-            try
-            {
-                _context.Ejemplos.Remove(DbObjeto);
-                await _context.SaveChangesAsync();
-
-                return Ok("Eliminado correctamente");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("No fué posible eliminar el objeto");
-            }
-
+            unidadRepository.DeleteUnidad(id);
+            return Ok("Unidad eliminada correctamente.");
         }
     }
 }
